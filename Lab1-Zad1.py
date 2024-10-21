808c51e5f1d668a62e99fdb883872bdaa5c67680
DawidStanisz <d.stanisz00@gmail.com>
2024-10-21 21:55:58 +0200

    lab1

---
+++ Lab1-Zad1.py
@@ -0,0 +1,17 @@
+#A
+print(type(1 + 2))      # "+" Dodawanie
+print(type(1 + 4.5))    # "+" Dodawanie
+print(type(3 / 2))      # "/" Dzielenie zmiennoprzecinkowe
+print(type(4 / 2))      # "/" Dzielenie zmiennoprzecinkowe
+print(type(3 // 2))     # "//" Dzielenie całkowite
+print(type(-3 // 2))    # "//" Dzielenie całkowite
+print(type(11 % 2))     # % Reszta z dzielenia
+print(type(2 ** 10))    # "**" Potęgowanie
+print(type(8 ** (1/3))) # "**" Potęgowanie
+
+#B
+print(int(3.0))     #Konwersja liczby zmiennoprzecinkowej(float) na całkowitą(int)
+print(float(3))     #Konwersja liczby całkowitej(int) na zmiennoprzecinkową(float)
+print(float("3"))   #Konwersja tekstu(string) na liczbę zmiennoprzecinkową(float)
+print(str(12.4))    #Konwersja liczby zmiennoprzecinkowej(float) na tekst(string)
+print(bool(0))      #Konwersja 0 na wartość logiczną(bool)
---
+++ Lab1-Zad4.py
@@ -0,0 +1,6 @@
+Cena=39.99
+Rabat=0.2
+
+Koncowa_cena = Cena * (1-Rabat)
+
+print(f"Cena po zastosowaniu rabatu: {Koncowa_cena:.2f} PLN")
