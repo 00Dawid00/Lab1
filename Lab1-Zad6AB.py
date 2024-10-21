7bf93e5bb42fbfc8087216ec1689a40da681bcac
DawidStanisz <d.stanisz00@gmail.com>
2024-10-21 23:18:30 +0200

    lab1

--- Lab1-Zad6AB.py
+++ Lab1-Zad6AB.py
@@ -3,8 +3,8 @@ import random
 droga=random.randint(0,1000)
 print(f"Losowo wygenerowana droga pokonana przez samochód: {droga}km")
 
-spalanie=float(input("Średnie spalanie (l/100km)"))
-cena_paliwa=float(input("Podaj cenę paliwa (zł/l)"))
+spalanie=float(input("Średnie spalanie (l/100km): "))
+cena_paliwa=float(input("Podaj cenę paliwa (zł/l): "))
 
 zuzycie_paliwa=(droga/100)*spalanie
 koszt_podrozy=zuzycie_paliwa*cena_paliwa
